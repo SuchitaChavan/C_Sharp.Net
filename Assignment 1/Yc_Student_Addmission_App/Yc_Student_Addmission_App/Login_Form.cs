3897b1665880fa1fc10fdfbb30afcbe3505b59dd
SuchitaChavan <158129439+SuchitaChavan@users.noreply.github.com>
2024-06-20 21:54:26 +0530

    Add files via upload

---
+++ Assignment 1/Yc_Student_Addmission_App/Yc_Student_Addmission_App/Login_Form.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yc_Student_Addmission_App
+{
+    public partial class frm_Login_Form : Form
+    {
+        public frm_Login_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Submit_Click(object sender, EventArgs e)
+        {
+            if(tb_Username.Text=="Admin" && tb_Password.Text=="123" || tb_Username.Text=="S" && tb_Password.Text=="s")
+            {
+                MessageBox.Show("Login Successful", "Welcome",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                frm_Add_New_Student obj = new frm_Add_New_Student();
+                obj.Show();
+                this.Hide();
+
+            }
+            else
+            {
+                MessageBox.Show("Enter Correct Username & Password", "Login failed", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                tb_Username.Clear();
+                tb_Password.Clear();
+
+            }
+        }
+
+   
+    }
+}
